de2bf4f04771c49714fde50c32341738750363c4
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-26 18:18:37 +0100

    Initialise project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<div class="flex flex-col px-4 max-w-7xl mx-auto min-h-screen">
+	<main>
+		<div class="flex-1 space-y-4 overflow-y-auto">
+			{@render children?.()}
+		</div>
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Page route</h1>
