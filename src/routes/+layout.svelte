04793be4e0e9cd86201216d32f0e97e38156af16
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-29 08:48:09 +0000

    Add fixed aside that does not scroll with main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,17 +9,23 @@
 </script>
 
 <div class="flex flex-col max-w-screen-2xl mx-auto">
-	<header class="bg-green-800 h-20 fixed top-0 max-w-screen-2xl mx-auto left-0 right-0">
+	<header class="bg-emerald-800 h-20 fixed top-0 max-w-screen-2xl mx-auto left-0 right-0">
 		<h1>Header</h1>
 	</header>
 
-	<main class="bg-blue-950 pt-24 pb-24">
-		<div class="space-y-4">
-			{@render children?.()}
-		</div>
-	</main>
+	<div class="flex pt-20 pb-20">
+		<aside class="bg-yellow-800 w-64 fixed top-20 bottom-20">
+			<h1>Aside</h1>
+		</aside>
 
-	<footer class="bg-red-800 h-20 fixed bottom-0 max-w-screen-2xl mx-auto left-0 right-0">
+		<main class="bg-blue-900 flex-1 ml-64">
+			<div class="space-y-4">
+				{@render children?.()}
+			</div>
+		</main>
+	</div>
+
+	<footer class="bg-pink-800 h-20 fixed bottom-0 max-w-screen-2xl mx-auto left-0 right-0">
 		<h1>Footer</h1>
 	</footer>
 </div>
