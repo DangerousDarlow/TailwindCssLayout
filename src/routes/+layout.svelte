40d408ee4037a9ab234c6c25daafe1e7d4f4c1e4
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-29 09:15:01 +0000

    Add reactivity. Aside is only displayed on large screens and footer is only displayed on small screens.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,23 +9,23 @@
 </script>
 
 <div class="flex flex-col max-w-screen-2xl mx-auto">
-	<header class="bg-emerald-800 h-20 fixed top-0 max-w-screen-2xl mx-auto left-0 right-0">
+	<header class="bg-emerald-800 fixed top-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto">
 		<h1>Header</h1>
 	</header>
 
-	<div class="flex pt-20 pb-20">
-		<aside class="bg-yellow-800 w-64 fixed top-20 bottom-20">
+	<div class="flex pt-20 pb-20 md:pb-0">
+		<aside class="bg-yellow-800 fixed top-20 bottom-0 w-64 hidden md:block">
 			<h1>Aside</h1>
 		</aside>
 
-		<main class="bg-blue-900 flex-1 ml-64">
+		<main class="bg-blue-900 flex-1 md:ml-64">
 			<div class="space-y-4">
 				{@render children?.()}
 			</div>
 		</main>
 	</div>
 
-	<footer class="bg-pink-800 h-20 fixed bottom-0 max-w-screen-2xl mx-auto left-0 right-0">
+	<footer class="bg-pink-800 fixed bottom-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto md:hidden">
 		<h1>Footer</h1>
 	</footer>
 </div>
