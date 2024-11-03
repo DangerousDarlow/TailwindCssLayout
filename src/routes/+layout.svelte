db770fc6836b60e15627175789cdb694d4413ac6
Nick Darlow <dangerous.darlow@gmail.com>
2024-11-03 12:11:21 +0000

    Add default background bg-default
    Add bg-default to force background color to the current default. Applying this means the background will not be transparent. See the footer.
    
    Change background opacity to better show overlap.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,25 +9,25 @@
 </script>
 
 <div class="flex flex-col max-w-screen-2xl mx-auto">
-	<header class="bg-emerald-800 fixed top-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto">
-		<div class="flex items-center h-full">
-			<h1 class="flex items-center justify-center inset-0 absolute">Header</h1>
-		</div>
+	<header class="bg-red-500 bg-opacity-40 fixed top-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto">
+		<h1 class="flex items-center justify-center inset-0 absolute">Header</h1>
 	</header>
 
-	<div class="flex pt-20 pb-20 md:pb-0">
-		<aside class="bg-yellow-800 fixed top-20 bottom-0 w-64 hidden md:block">
+	<div class="flex pt-20 pb-20">
+		<aside
+			class="bg-green-500 bg-opacity-40 fixed top-20 bottom-0 md:bottom-20 w-64 hidden md:block"
+		>
 			<h1>Aside</h1>
 		</aside>
 
-		<main class="bg-blue-900 flex-1 md:ml-64">
+		<main class="bg-blue-500 bg-opacity-40 flex-1 md:ml-64">
 			<div class="space-y-4">
 				{@render children?.()}
 			</div>
 		</main>
 	</div>
 
-	<footer class="bg-pink-800 fixed bottom-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto md:hidden">
-		<h1>Footer</h1>
+	<footer class="bg-default fixed bottom-0 left-0 right-0 h-20 max-w-screen-2xl mx-auto">
+		<h1 class="flex items-center justify-center inset-0 absolute">Footer</h1>
 	</footer>
 </div>
